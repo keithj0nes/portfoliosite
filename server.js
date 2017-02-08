0897f1cb30a6876dfd886f478b5cd47750afd8b3
keith <keith@raincityambience.com>
2017-02-07 22:09:20 -0700

    add resume & smarttable

--- server.js
+++ server.js
@@ -6,3 +6,6 @@ app.use(express.static(__dirname + '/public'));
 app.listen(3011, function(){
   console.log("listening on 3011");
 })
+
+// save front end changes -> run gulp
+// save back end changes -> forever restartall
